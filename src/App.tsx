e73bdab67d05d410501b895ef6a912d6e277e8d7
Keshav Nischal <91429557+keshav-nischal@users.noreply.github.com>
2024-09-29 18:49:28 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AuthenticationPage from './Pages/Authentication'
+
+function App() {
+
+  return (
+    <>
+      <AuthenticationPage></AuthenticationPage>
+    </>
+  )
+}
+
+export default App
